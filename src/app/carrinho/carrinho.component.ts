a34b56d85f20096e9f139c827664e67abf6df560
italomaia03 <italo.fernandes.om@gmail.com>
2023-02-09 17:11:45 -0300

    implement the logic of the cart component

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
-  styleUrls: ['./carrinho.component.css']
+  styleUrls: ['./carrinho.component.css'],
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calcularTotal();
+  }
+
+  removerProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(
+      (item) => item.id !== produtoId
+    );
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
+
+  calcularTotal() {
+    this.total = 0;
+    for (let item of this.itensCarrinho) {
+      this.total += item.preco * item.quantidade;
+    }
+  }
+
+  comprar() {
+    this.carrinhoService.limparCarrinho();
+    alert('Parabéns! Sua compra foi realizada com sucesso.');
+    this.router.navigate(['produtos']);
+  }
 }
