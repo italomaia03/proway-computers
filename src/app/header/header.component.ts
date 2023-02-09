3e25dfcf2654588fa750af7390f393a58572799b
italomaia03 <italo.fernandes.om@gmail.com>
2023-02-09 14:50:52 -0300

    create cart route and its template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  constructor(public carrinhoService: CarrinhoService) {}
 }
