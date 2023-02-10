411a4c991b722aa752f16c747d75fb3d774a9a2f
italomaia03 <italo.fernandes.om@gmail.com>
2023-02-10 15:26:09 -0300

    implement search functionality

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduto, produtos } from '../produtos';
 import { ProdutosService } from '../produtos.service';
 
@@ -10,8 +11,23 @@ import { ProdutosService } from '../produtos.service';
 export class ProdutosComponent implements OnInit {
   produtos: IProduto[] | undefined;
 
-  constructor(private produtosService: ProdutosService) {}
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.produtos = this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe((params) => {
+      const descricao = params.get('descricao')?.toLowerCase();
+
+      if (descricao) {
+        this.produtos = produtos.filter((produto) =>
+          produto.descricao.toLowerCase().includes(descricao)
+        );
+        return;
+      }
+
+      this.produtos = produtos;
+    });
   }
 }
